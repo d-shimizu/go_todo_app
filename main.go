7ecb7c464f6febaf286d57974bc39b8a8b3179fb
d-shimizu <daisuke.shimizu.ds@gmail.com>
2023-05-09 02:06:33 +0000

    chapter16/section-061 シグナルをハンドリングする

--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/d-shimizu/go_todo_app_hands-on/config"
 	"golang.org/x/sync/errgroup"
@@ -20,6 +23,9 @@ func main() {
 }
 
 func run(ctx context.Context) error {
+	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
 	cfg, err := config.New()
 	if err != nil {
 		return err
@@ -33,6 +39,8 @@ func run(ctx context.Context) error {
 		// 引数で受け取ったnet.Listenerを利用するので、
 		// Addr フィールドは指定しない
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			// コマンドラインで実験するため
+			time.Sleep(5 * time.Second)
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 		}),
 	}
