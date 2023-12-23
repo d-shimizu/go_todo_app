3cf7e0c6d285e25475b57be5c70fe363154c701f
d-shimizu <2187802+d-shimizu@users.noreply.github.com>
2023-12-23 17:41:12 +0000

    section-073 RDBMSに対する操作を実装する

---
+++ store/repository.go
@@ -0,0 +1,68 @@
+package store
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"time"
+
+	"github.com/d-shimizu/go_todo_app_hands-on/config"
+	"github.com/jmoiron/sqlx"
+)
+
+type Beginner interface {
+	BeginTx(ctx context.Context, opts *sql.TxOptions) (*sqlx.Tx, error)
+}
+
+type Preparer interface {
+	Preparex(ctx context.Context, query string) (*sqlx.Stmt, error)
+}
+
+type Execer interface {
+	ExecContext(ctx context.Context, query string, args ...any) (sql.Result, error)
+	NamedExecContext(ctx context.Context, query string, arg interface{}) (sql.Result, error)
+}
+
+type Queryer interface {
+	Preparer
+	QueryContext(ctx context.Context, query string, args ...any) (*sql.Rows, error)
+	QueryRowContext(ctx context.Context, query string, args ...any) *sql.Row
+	GetContext(ctx context.Context, dest interface{}, query string, args ...any) error
+	SelectContext(ctx context.Context, dest interface{}, query string, args ...any) error
+}
+
+var (
+	_ Beginner = (*sqlx.DB)(nil)
+	_ Preparer = (*sqlx.DB)(nil)
+	_ Queryer  = (*sqlx.DB)(nil)
+	_ Execer   = (*sqlx.DB)(nil)
+	_ Execer   = (*sqlx.Tx)(nil)
+)
+
+type Repository struct {
+	Clocker clock.Clocker
+}
+
+func New(ctx context.Context, cfg *config.Config) (*sqlx.DB, func(), error) {
+	// slqx.Connect を使うと内部でPingする
+	db, err := sql.Open("mysql",
+		fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?parseTime=true",
+			cfg.DBUser, cfg.DBPassword,
+			cfg.DBHost, cfg.DBPort,
+			cfg.DBName,
+		),
+	)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// Openは実際には接続を行わないので、Pingで接続を確認する
+	ctx, cancel := context.WithTimeout(ctx, 2*time.Second)
+	defer cancel()
+	if err := db.PingContext(ctx); err != nil {
+		return nil, func() { _ = db.Close() }, err
+	}
+
+	xdb := sqlx.NewDb(db, "mysql")
+	return xdb, func() { _ = db.Close() }, nil
+}
